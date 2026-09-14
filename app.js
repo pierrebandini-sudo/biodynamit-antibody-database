77551e2b6301bb514dcfd6f636521dcad17d0dd2
pierrebandini-sudo <pierre.bandini@gmail.com>
2026-09-14 11:48:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/controls/OrbitControls.js';
-
+import * as THREE from 'https://esm.sh/three@0.180.0';
+import { OrbitControls } from 'https://esm.sh/three@0.180.0/examples/jsm/controls/OrbitControls.js';
 const CANON = {
   Suppliers: [
     {id:'Name', type:'Text'}, {id:'Website', type:'Text'}, {id:'RawVariants', type:'Text'}
